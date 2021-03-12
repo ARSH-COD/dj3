d6e8d96929d9c6a88f8c8fcb7baac7f8ee8371b8
ARSH-COD <80031614+ARSH-COD@users.noreply.github.com>
2021-03-12 18:02:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ leftWristY=0;
 rightWristX=0;
 rightWristY=0;
 song="";
+scoreleftwrist=0;
+scorerighttwrist=0;
 function preload(){
     song=loadSound("music.mp3");
 }
@@ -18,12 +20,40 @@ function draw(){
     image(video,0,0,600,500);
     fill("#FF0000");
     stroke("#FF0000");
-    circle(leftWristX,leftWristY,20);
-    innumberleftWristY=Number(leftWristY);
-    removedecimals=floor(innumberleftWristY);
-    volume=removedecimals/500;
-    document.getElementById("volume").innerHTML="volume-"+volume;
-    song.setVolume(volume);
+    if(scorerighttwrist>0.2){
+        circle(rightWristX,rightWristY,20);
+        if(rightWristY>0 && rightWristY<=100){
+        document.getElementById("speed").innerHTML="speed=0.5x";
+        song.rate(0.5);
+        }
+        else if(rightWristY>100 && rightWristY<=200){
+            document.getElementById("speed").innerHTML="speed=1x";
+            song.rate(1);
+    
+        }
+        else if(rightWristY>200 && rightWristY<=300){
+            document.getElementById("speed").innerHTML="speed=1.5x";
+            song.rate(1.5);
+        }
+        else if(rightWristY>300 && rightWristY<=400){
+            document.getElementById("speed").innerHTML="speed=2x";
+            song.rate(2);
+        }
+        else if(rightWristY>400 && rightWristY<=500){
+            document.getElementById("speed").innerHTML="speed=2.5x";
+            song.rate(2.5);
+        }
+    }
+  
+    if(scoreleftwrist>0.2){
+        circle(leftWristX,leftWristY,20);
+        innumberleftWristY=Number(leftWristY);
+        removedecimals=floor(innumberleftWristY);
+        volume=removedecimals/500;
+        document.getElementById("volume").innerHTML="volume-"+volume;
+        song.setVolume(volume); 
+    }    
+    
 }
 function play(){
     song.play();
@@ -35,7 +65,7 @@ console.log("posenet is initialized");
 }
 function gotPoses(results)
 {
-if (results.lengthfv>0){
+if (results.length>0){
     console.log(results);
  leftWristX=results[0].pose.leftWrist.x;
  leftWristY=results[0].pose.leftWrist.y;
@@ -43,5 +73,8 @@ if (results.lengthfv>0){
  rightWristX=results[0].pose.rightWrist.x;
  rightWristY=results[0].pose.rightWrist.y;
  console.log("rightWristX= "+rightWristX+" rightWristY= "+ rightWristY);
+scoreleftwrist=results[0].pose.keypoints[9].score;
+scorerighttwrist=results[0].pose.keypoints[10].score;
+console.log("scoreleftwrist= "+scoreleftwrist+" scorerightwrist="+ scorerighttwrist);
 }
 }
